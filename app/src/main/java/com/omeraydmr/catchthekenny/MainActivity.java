dc2082c4ca03c877eafbfa20d6f1f1f744c824d8
Ömer Aydemir <osilix1@gmail.com>
2021-08-25 14:59:40 +0300

    Changing visibility of ImageView.

--- app/src/main/java/com/omeraydmr/catchthekenny/MainActivity.java
+++ app/src/main/java/com/omeraydmr/catchthekenny/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,6 +13,16 @@ public class MainActivity extends AppCompatActivity {
     TextView scoreBoard;
     TextView timer;
     int score;
+    ImageView imageView;
+    ImageView imageView2;
+    ImageView imageView3;
+    ImageView imageView4;
+    ImageView imageView5;
+    ImageView imageView6;
+    ImageView imageView7;
+    ImageView imageView8;
+    ImageView imageView9;
+    ImageView[] imageArray;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +32,20 @@ public class MainActivity extends AppCompatActivity {
         //initialize
         timer = (TextView) findViewById(R.id.timer);
         scoreBoard = (TextView) findViewById(R.id.scoreBoard);
+        imageView = findViewById(R.id.imageView);
+        imageView2 = findViewById(R.id.imageView2);
+        imageView3 = findViewById(R.id.imageView3);
+        imageView4 = findViewById(R.id.imageView4);
+        imageView5 = findViewById(R.id.imageView5);
+        imageView6 = findViewById(R.id.imageView6);
+        imageView7 = findViewById(R.id.imageView7);
+        imageView8 = findViewById(R.id.imageView8);
+        imageView9 = findViewById(R.id.imageView9);
+
+        imageArray = new ImageView[] {imageView, imageView2, imageView3, imageView4, imageView5, imageView6, imageView7, imageView8, imageView9};
+
+        hideImages();
+
         score = 0;
 
         new CountDownTimer(10000,1000) {
@@ -41,5 +66,12 @@ public class MainActivity extends AppCompatActivity {
     public void increaseScore(View view) {
         score++;
         scoreBoard.setText("Score : "+ score);
+    }
+
+    public void hideImages() {
+
+        for(ImageView image : imageArray) {
+            image.setVisibility(View.INVISIBLE);
+        }
     }
 }
