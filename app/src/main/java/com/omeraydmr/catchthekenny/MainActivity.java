a5eda5d3e706b5ee67581b40220ae924cde10bd7
Ömer Aydemir <osilix1@gmail.com>
2021-08-25 14:41:46 +0300

    Increasing score added to game.

--- app/src/main/java/com/omeraydmr/catchthekenny/MainActivity.java
+++ app/src/main/java/com/omeraydmr/catchthekenny/MainActivity.java
@@ -3,12 +3,29 @@ package com.omeraydmr.catchthekenny;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView scoreBoard;
+    TextView timer;
+    int score;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //initialize
+        timer = (TextView) findViewById(R.id.timer);
+        scoreBoard = (TextView) findViewById(R.id.scoreBoard);
+        score = 0;
+
+    }
+
+    public void increaseScore(View view) {
+        score++;
+        scoreBoard.setText("Score : "+ score);
     }
 }
